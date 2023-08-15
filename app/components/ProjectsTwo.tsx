47384eb054299a6fb33ef5fbc11f26e56c659292
LawrenceDarko <darkolawrence@gmail.com>
2023-08-15 12:20:09 +0000

    new commit

--- app/components/ProjectsTwo.tsx
+++ app/components/ProjectsTwo.tsx
@@ -22,7 +22,8 @@ const ProjectsTwo = () => {
     const descriptions = {
         desc1: 'VeganEats" is a stylish food ordering app designed for vegans. With a sleek, dark blue theme, it offers a seamless experience for users. The app consists of dedicated components such as a Riders App, Users App, Restaurant Dashboard, and Main Admin Dashboard. It can be easily accessed and installed through various platforms, catering to the needs of developers and enthusiasts alike.',
         desc2: 'A cutting-edge website for Carisca Lab, a supply chain innovation lab. The website highlights their team of skilled developers and programmers, who work on groundbreaking solutions for supply chain optimization. It showcases their innovative projects, expertise, and services, attracting potential partners and clients in the industry.',
-        desc3: '"NetflixClone" is my fun and boredom-inspired project, a lighthearted replica of the popular streaming platform. It showcases my coding skills and creativity, offering a playful and unique experience. Sit back, relax, and dive into the world of my homemade streaming platform!'
+        desc3: '"NetflixClone" is my fun and boredom-inspired project, a lighthearted replica of the popular streaming platform. It showcases my coding skills and creativity, offering a playful and unique experience. Sit back, relax, and dive into the world of my homemade streaming platform!',
+        desc4: "'PaymentLink' is user-friendly software for easy payment collection through shared links. It simplifies group payments, bill splitting, and event organization. Customized links streamline transactions, making the process hassle-free. Experience simplified transactions with friends through PaymentLink, ending complex payment procedures."
     }
 
     return (
@@ -36,6 +37,7 @@ const ProjectsTwo = () => {
             <ProjectTwoItem image='cariscaa.png' title="CARISCA Lab" description={descriptions.desc2}/>
             <ProjectTwoItem showRight image="vegancol.png" title="FindVegan" description={descriptions.desc1}/>
             <ProjectTwoItem image="netflix.png" title="Netflix Clone" description={descriptions.desc3} githublink="https://github.com/LawrenceDarko/netflix-clone"/>
+            <ProjectTwoItem showRight image="paymentlink.png" title="Payment Pages" description={descriptions.desc4} githublink="https://github.com/LawrenceDarko/netflix-clone"/>
             <div ref={ref} className="flex justify-end w-full">
                 <motion.div
                     variants={{
